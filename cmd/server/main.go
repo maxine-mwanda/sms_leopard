e9e73c052109a2463094ab7b8e3aca4485e843c8
Maxine <mwandamaxyine@gmail.com>
2025-12-07 22:01:23 +0300

    added logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,91 +1,93 @@
 package main
 
 import (
-    //"database/sql"
-    "github.com/joho/godotenv"
-    "log"
-    "net/http"
-    "os"
-    "time"
+	//"database/sql"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
-    "github.com/streadway/amqp"
-    "github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 
-    "sms_leopard/controllers"
-    dbpkg "sms_leopard/db"
-    "sms_leopard/models"
-    "sms_leopard/queue"
-    workerpkg "sms_leopard/worker"
+	"github.com/gorilla/mux"
+	"github.com/streadway/amqp"
+
+	"sms_leopard/controllers"
+	dbpkg "sms_leopard/db"
+	"sms_leopard/models"
+	"sms_leopard/queue"
+	workerpkg "sms_leopard/worker"
 )
 
 func main() {
-    // Load env
-    godotenv.Load()
+	// Load env
+	godotenv.Load()
 
-    // Database setup
-    dsn := os.Getenv("DSN")
-    if dsn == "" {
-        log.Fatal("DSN env required")
-    }
-    sqlDB, err := dbpkg.OpenFromDSN(dsn)
-    if err != nil {
-        log.Fatalf("db open: %v", err)
-    }
-    if err := dbpkg.Migrate(sqlDB); err != nil {
-        log.Fatalf("migrate: %v", err)
-    }
-    svc := models.NewService(sqlDB)
+	// Database setup
+	dsn := os.Getenv("DSN")
+	if dsn == "" {
+		log.Fatal("DSN env required")
+	}
+	sqlDB, err := dbpkg.OpenFromDSN(dsn)
+	if err != nil {
+		log.Fatalf("db open: %v", err)
+	}
+	if err := dbpkg.Migrate(sqlDB); err != nil {
+		log.Fatalf("migrate: %v", err)
+	}
+	svc := models.NewService(sqlDB)
 
-    // AMQP setup
-    amqpURL := os.Getenv("AMQP_URL")
-    if amqpURL == "" {
-        log.Fatal("AMQP_URL env required")
-    }
-    conn, err := amqp.Dial(amqpURL)
-    if err != nil {
-        log.Fatalf("amqp dial: %v", err)
-    }
-    defer conn.Close()
+	// AMQP setup
+	amqpURL := os.Getenv("AMQP_URL")
+	if amqpURL == "" {
+		log.Fatal("AMQP_URL env required")
+	}
+	conn, err := amqp.Dial(amqpURL)
+	if err != nil {
+		log.Fatalf("amqp dial: %v", err)
+	}
+	defer conn.Close()
 
-    pub, err := queue.NewPublisher(conn, "smsleopard-exchange")
-    if err != nil {
-        log.Fatalf("publisher: %v", err)
-    }
-    consumer, err := queue.NewConsumer(conn, "smsleopard-queue")
-    if err != nil {
-        log.Fatalf("consumer: %v", err)
-    }
+	pub, err := queue.NewPublisher(conn, "smsleopard-exchange")
+	if err != nil {
+		log.Fatalf("publisher: %v", err)
+	}
+	consumer, err := queue.NewConsumer(conn, "smsleopard-queue")
+	if err != nil {
+		log.Fatalf("consumer: %v", err)
+	}
 
-    // Handlers
-    handler := controllers.NewHandler(svc, pub)
-    r := mux.NewRouter()
+	// Handlers
+	handler := controllers.NewHandler(svc, pub)
+	r := mux.NewRouter()
 
-    // Campaign endpoints
-    r.HandleFunc("/campaigns", handler.CreateCampaign).Methods("POST")
-    r.HandleFunc("/campaigns", handler.ListCampaigns).Methods("GET")
-    r.HandleFunc("/campaigns/{id:[0-9]+}", handler.GetCampaignDetails).Methods("GET")
-    r.HandleFunc("/campaigns/{id:[0-9]+}/send", handler.SendCampaign).Methods("POST")
-    r.HandleFunc("/campaigns/{id:[0-9]+}/personalized-preview", handler.Preview).Methods("POST")
+	// Campaign endpoints
+	r.HandleFunc("/campaigns", handler.CreateCampaign).Methods("POST")
+	r.HandleFunc("/campaigns", handler.ListCampaigns).Methods("GET")
+	r.HandleFunc("/campaigns/{id:[0-9]+}", handler.GetCampaignDetails).Methods("GET")
+	r.HandleFunc("/campaigns/{id:[0-9]+}/send", handler.SendCampaign).Methods("POST")
+	r.HandleFunc("/campaigns/{id:[0-9]+}/personalized-preview", handler.Preview).Methods("POST")
 
-    // Stats & health
-    r.HandleFunc("/stats", handler.Stats).Methods("GET")
-    r.HandleFunc("/health", handler.Health).Methods("GET")
+	// Stats & health
+	r.HandleFunc("/stats", handler.Stats).Methods("GET")
+	r.HandleFunc("/health", handler.Health).Methods("GET")
 
-    // Start worker
-    worker := workerpkg.NewWorker(svc, consumer)
-    go func() {
-        if err := worker.Start(); err != nil {
-            log.Printf("worker err: %v", err)
-        }
-    }()
+	// Start worker
+	worker := workerpkg.NewWorker(svc, consumer)
+	go func() {
+		if err := worker.Start(); err != nil {
+			log.Printf("worker err: %v", err)
+		}
+	}()
 
-    srv := &http.Server{
-        Addr:         ":8080",
-        Handler:      r,
-        ReadTimeout:  10 * time.Second,
-        WriteTimeout: 10 * time.Second,
-    }
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
 
-    log.Println("listening :8080")
-    log.Fatal(srv.ListenAndServe())
+	log.Println("listening :8080")
+	log.Fatal(srv.ListenAndServe())
+
 }
--- go.mod
+++ go.mod
@@ -6,8 +6,15 @@ require (
 	github.com/go-sql-driver/mysql v1.9.3
 	github.com/gorilla/mux v1.8.1
 	github.com/joho/godotenv v1.5.1
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/mattn/go-sqlite3 v1.14.32
 	github.com/streadway/amqp v1.1.0
 )
 
-require filippo.io/edwards25519 v1.1.0 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/jonboulle/clockwork v0.5.0 // indirect
+	github.com/lestrrat-go/strftime v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
+)
